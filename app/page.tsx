1d55494a31711835e78969763bc4c10b41b0550d
Nishat Sarker <141291102+uqnsarke@users.noreply.github.com>
2025-11-29 16:38:33 -0500

    Add Navbar and Services components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -20,3 +20,17 @@ export default function Home() {
     </main>
   );
 }
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Services />
+    </main>
+  );
+}
