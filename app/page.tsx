f0621104562a84bd3b92379e1626fcecbffec2d1
Nishat Sarker <141291102+uqnsarke@users.noreply.github.com>
2025-11-29 17:37:52 -0500

    Refactor Home component to include Services

--- app/page.tsx
+++ app/page.tsx
@@ -2,31 +2,6 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 
-
-export default function Home() {
-  return (
-    <main>
-      <Navbar />
-      <Hero />
-    </main>
-  );
-}
-
-
-import Hero from "./components/Hero";
-
-export default function Home() {
-  return (
-    <main>
-      <Hero />
-    </main>
-  );
-}
-
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Services from "./components/Services";
-
 export default function Home() {
   return (
     <main>
