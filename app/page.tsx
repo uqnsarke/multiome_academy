db43dc9f43a697fad170a24e70f98fa640f16635
Nishat Sarker <141291102+uqnsarke@users.noreply.github.com>
2025-11-30 19:54:53 -0500

    Fix main tag structure in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import Services from "./components/Services";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black text-white">
-      <h1 className="text-6xl p-20">MULTIOME ACADEMY</h1>
+    <main>
       <Navbar />
       <Hero />
       <Services />
