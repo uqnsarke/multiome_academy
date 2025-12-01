e5ee4210b7743d94dfb0f2701e60a05ced90b8c2
Nishat Sarker <141291102+uqnsarke@users.noreply.github.com>
2025-11-30 19:49:35 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Services from "./components/Services";
-
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen bg-black text-white">
+      <h1 className="text-6xl p-20">MULTIOME ACADEMY</h1>
       <Navbar />
       <Hero />
       <Services />
