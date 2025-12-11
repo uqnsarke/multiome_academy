3d1c260fc5fde437dc26ca63ee3ca383a395a91e
Nishat Sarker <141291102+uqnsarke@users.noreply.github.com>
2025-12-10 21:13:46 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,15 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ReactNode } from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "Multiome Academy",
-  description: "Learn single-cell, multiome, and AI for biology from scratch.",
+  description: "Learn Multiome Analysis From Scratch"
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
