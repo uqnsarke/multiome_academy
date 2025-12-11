0cf5f7c841615cc5785b79c0485e8dac1e78ee40
Nishat Sarker <141291102+uqnsarke@users.noreply.github.com>
2025-12-10 22:42:58 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,10 @@
-module.exports = { content: ['./app/**/*.{ts,tsx}','./components/**/*.{ts,tsx}'], theme:{extend:{}}, plugins:[] };
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
