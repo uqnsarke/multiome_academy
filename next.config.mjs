a6cbb216141d38ad7bfcf99317047cfb8c92aae1
Nishat Sarker <141291102+uqnsarke@users.noreply.github.com>
2025-12-20 14:15:19 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,14 @@
-export default { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  experimental: {
+    turbo: true,
+  },
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+};
+
+export default nextConfig;
