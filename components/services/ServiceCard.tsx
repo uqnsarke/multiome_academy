9f791a9b23c220743a9879e0433a62d2e52b39aa
Nishat Sarker <141291102+uqnsarke@users.noreply.github.com>
2025-12-21 16:10:52 -0500

    Update ServiceCard.tsx

--- components/services/ServiceCard.tsx
+++ components/services/ServiceCard.tsx
@@ -10,18 +10,15 @@ interface ServiceCardProps {
 
 export default function ServiceCard({ front, back, flipped }: ServiceCardProps) {
   return (
-    <div className="relative w-full h-80 perspective cursor-pointer group">
+    <div className="relative w-full h-[360px] md:h-[380px] perspective cursor-pointer group">
       <div 
         className={`relative w-full h-full duration-700 transform-style-3d ${
           flipped ? 'rotate-y-180' : ''
         }`}
       >
-        {/* Front Side Layer */}
         <div className="absolute w-full h-full backface-hidden">
           {front}
         </div>
-
-        {/* Back Side Layer */}
         <div className="absolute w-full h-full backface-hidden rotate-y-180">
           {back}
         </div>
